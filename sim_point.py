fd72043b11fa955c216d6d79ca2b9e91aee498f9
Thassilo Schulze <thassilo@thasso.xyz>
2025-02-23 10:28:45 +0100

    Switch to np arrays

--- sim_point.py
+++ sim_point.py
@@ -1,6 +1,8 @@
 from enum import Enum, auto
 from typing import Tuple
 import math
+import numpy as np
+from numpy.typing import NDArray
 
 class Element(Enum):
     HELIUM = auto() # helium-4
@@ -54,27 +56,22 @@ def planck_func(wavelength: float, temp: float) -> float:
     k_B = 1.380649e-23  # Boltzmann constant (J/K)
     return 2 * h * c**2 / wavelength**5 / (math.exp(h * c / (wavelength * k_B * temp)) - 1)
 
-def calc_spectra(dist_by_layer: list[list[Tuple[Element, float]]], wavelengths: list[float], depth: float, temp: float) -> Tuple[list[list[float]], list[list[float]]]:
-    emission_spectra = []
-    transmitted_spectra = []
+def calc_spectra(dist_by_layer: list[list[Tuple[Element, float]]], wavelengths: NDArray[np.float64], depth: float, temp: float) -> Tuple[NDArray[np.float64], NDArray[np.float64]]:
+    emission_spectra = np.zeros((len(dist_by_layer), len(wavelengths)))
+    transmitted_spectra = np.zeros((len(dist_by_layer), len(wavelengths)))
 
-    for dist in dist_by_layer:
-        emission_spectrum = []
-        transmitted_spectrum = []
-        for wavelength in wavelengths:
+    for i, dist in enumerate(dist_by_layer):
+        for j, wavelength in enumerate(wavelengths):
             absorption_coeff = sum(frac * elem.absorption_cross_section(wavelength) for elem, frac in dist)
             optical_depth = absorption_coeff * depth
             transmission = math.exp(-optical_depth)
             absorption = 1 - transmission
-            emission_spectrum.append(absorption * planck_func(wavelength, temp))
-            transmitted_spectrum.append(transmission)
-
-        emission_spectra.append(emission_spectrum)
-        transmitted_spectra.append(transmitted_spectrum)
+            emission_spectra[i, j] = absorption * planck_func(wavelength, temp)
+            transmitted_spectra[i, j] = transmission
 
     return emission_spectra, transmitted_spectra
 
-def sim_point(temp: float, surface_pressure: float, elements: list[Tuple[Element, float]], wavelengths: list[float], incident_spectrum: list[float], heights: list[float], depth: float) -> list[float]:
+def sim_point(temp: float, surface_pressure: float, elements: list[Tuple[Element, float]], wavelengths: NDArray[np.float64], incident_spectrum: NDArray[np.float64], heights: NDArray[np.float64], depth: float) -> NDArray[np.float64]:
     assert sum(cont for _, cont in elements) == 1.0
     assert len(wavelengths) == len(incident_spectrum)
 
@@ -82,23 +79,25 @@ def sim_point(temp: float, surface_pressure: float, elements: list[Tuple[Element
 
     emission_spectra, transmitted_spectra = calc_spectra(dist_by_layer, wavelengths, depth, temp)
 
+    assert(len(emission_spectra) == len(transmitted_spectra))
+
     #  For each layer, the spectrum that's transmitted through all layers above
-    transmitted_above = []
-    for layer_idx, emission_spectrum in enumerate(emission_spectra):
-        assert len(emission_spectrum) == len(wavelengths)
-        transmitted_above.append([emitted * math.prod(map(lambda ts: ts[wavelength_idx], transmitted_spectra[layer_idx + 1:])) for wavelength_idx, emitted in enumerate(emission_spectrum)])
+    transmitted_above = np.zeros((len(emission_spectra), len(wavelengths)))
+    for i, emission_spectrum in enumerate(emission_spectra):
+        transmitted_above[i] = emission_spectrum.copy()
+        for j in range(i, len(emission_spectra)):
+            transmitted_above[i] *= transmitted_spectra[j]
 
     # This is how much of the stars spectrum reaches the surface
     incident_surface_spectrum = incident_spectrum.copy()
     for transmission_spectrum in transmitted_spectra:
-        for idx, transmitted in enumerate(transmission_spectrum):
-            incident_surface_spectrum[idx] *= transmitted
+        incident_surface_spectrum *= transmission_spectrum
 
-    spectrum = incident_surface_spectrum # The spectrum reflected from the surface acts as our base
-    spectrum = [0.0 for _ in range(len(wavelengths))]
+    # Update to use numpy:
+    # spectrum = incident_surface_spectrum
+    spectrum = np.zeros(len(wavelengths))
     for layer in transmitted_above:
-        for idx, transmitted in enumerate(layer):
-            spectrum[idx] += transmitted
+        spectrum += layer
 
     assert len(spectrum) == len(wavelengths)
 
