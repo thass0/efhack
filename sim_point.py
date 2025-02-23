ffe6eb7ad251954b24e1d6d3f2d055274dc6e8b9
Thassilo Schulze <thassilo@thasso.xyz>
2025-02-23 10:02:05 +0100

    Small updates

--- sim_point.py
+++ sim_point.py
@@ -43,7 +43,7 @@ def pressure(temp: float, height: float, mean_molecular_mass: float, gravity: fl
 
 GRAVITY = 9.81
 
-def elem_dist_by_height(height: float, temp: float, surface_pressure: float, elements: list[Element]) -> list[Tuple[Element, float]]:
+def calc_elem_dist(height: float, temp: float, surface_pressure: float, elements: list[Element]) -> list[Tuple[Element, float]]:
     pressures = [pressure(temp, height, elem.molecular_mass, GRAVITY, surface_pressure) for elem in elements]
     total_pressure = sum(pressures)
     return [(elem, pressure / total_pressure if total_pressure != 0.0 else 0.0) for elem, pressure in zip(elements, pressures)]
@@ -54,7 +54,7 @@ def planck_func(wavelength: float, temp: float) -> float:
     k_B = 1.380649e-23  # Boltzmann constant (J/K)
     return 2 * h * c**2 / wavelength**5 / (math.exp(h * c / (wavelength * k_B * temp)) - 1)
 
-def calc_spectra(dist_by_layer: list[list[Tuple[Element, float]]], wavelengths: list[float], layer_depth: float, temp: float) -> Tuple[list[list[float]], list[list[float]]]:
+def calc_spectra(dist_by_layer: list[list[Tuple[Element, float]]], wavelengths: list[float], depth: float, temp: float) -> Tuple[list[list[float]], list[list[float]]]:
     emission_spectra = []
     transmitted_spectra = []
 
@@ -63,7 +63,7 @@ def calc_spectra(dist_by_layer: list[list[Tuple[Element, float]]], wavelengths: 
         transmitted_spectrum = []
         for wavelength in wavelengths:
             absorption_coeff = sum(frac * elem.absorption_cross_section(wavelength) for elem, frac in dist)
-            optical_depth = absorption_coeff * layer_depth # TODO: Change layer_depth here depending on angle of incoming ray. Assuming 90 degrees to surface now.
+            optical_depth = absorption_coeff * depth
             transmission = math.exp(-optical_depth)
             absorption = 1 - transmission
             emission_spectrum.append(absorption * planck_func(wavelength, temp))
@@ -78,7 +78,7 @@ def sim_point(temp: float, surface_pressure: float, elements: list[Tuple[Element
     assert sum(cont for _, cont in elements) == 1.0
     assert len(wavelengths) == len(incident_spectrum)
 
-    dist_by_layer = [elem_dist_by_height(height, temp, surface_pressure, list(map(lambda x: x[0], elements))) for height in heights]
+    dist_by_layer = [calc_elem_dist(height, temp, surface_pressure, list(map(lambda x: x[0], elements))) for height in heights]
 
     emission_spectra, transmitted_spectra = calc_spectra(dist_by_layer, wavelengths, depth, temp)
 
@@ -94,7 +94,7 @@ def sim_point(temp: float, surface_pressure: float, elements: list[Tuple[Element
         for idx, transmitted in enumerate(transmission_spectrum):
             incident_surface_spectrum[idx] *= transmitted
 
-    # spectrum = incident_surface_spectrum # The spectrum reflected from the surface acts as our base
+    spectrum = incident_surface_spectrum # The spectrum reflected from the surface acts as our base
     spectrum = [0.0 for _ in range(len(wavelengths))]
     for layer in transmitted_above:
         for idx, transmitted in enumerate(layer):
@@ -103,28 +103,3 @@ def sim_point(temp: float, surface_pressure: float, elements: list[Tuple[Element
     assert len(spectrum) == len(wavelengths)
 
     return spectrum
-
-
-def calc_and_show():
-    temp = 270.0
-    surface_pressure = 1.0
-    elements = [(Element.HELIUM, 0.7), (Element.OXYGEN, 0.1), (Element.IRON, 0.2)]
-
-    wavelengths = [i + 400.0 for i in range(400)]
-    incident_spectrum = [1.0 for _ in range(400)]
-    depth = 10.0e8
-    heights = [i * depth for i in range(100)]
-
-    spectrum = sim_point(temp, surface_pressure, elements, wavelengths, incident_spectrum, heights, depth)
-
-    import matplotlib.pyplot as plt
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.plot(wavelengths, spectrum, linewidth=2)
-    ax.set_title('Spectrum Intensity')
-    ax.set_xlabel('Wavelength (nm)')
-    ax.set_ylabel('Intensity')
-    ax.set_xlim(380, 780)
-    ax.grid(True)
-    plt.show()
-
-calc_and_show()
